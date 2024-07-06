1120579669e63826893f071b8efe1dbe2d5d2d31
vishal-1372 <mvishal287@gmail.com>
2024-07-06 10:42:51 +0530

    first commit

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> list = new List<Student>() {
+        new Student() {
+                    Id = 1,
+                    Name = "Vishal",
+                    Desc = "Rajkot",
+
+            },
+            new Student() {
+                    Id = 2,
+                    Name = "Milan",
+                    Desc = "morbi",
+
+            },
+            new Student() {
+                    Id = 3,
+                    Name = "Khan",
+                    Desc = "surat",
+
+            },
+
+            new Student() {
+                    Id = 4,
+                    Name = "krushal",
+                    Desc = "morbi",
+
+            },
+    };
+            ViewBag.model = list;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewData["Country"] = new List<String>() { "India", "Pakistan","Uk","Us","Rus" };
+
+
+            
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Desc { get; set; }
+    }
+}
